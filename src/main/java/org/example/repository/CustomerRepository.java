e8ab6511f927efa144bc6ee5ca330ab20b7993c6
Chathura-Nilaksha <cnilakshami@gmail.com>
2024-11-10 19:27:08 +0530

    Developed several classes

--- src/main/java/org/example/repository/CustomerRepository.java
+++ src/main/java/org/example/repository/CustomerRepository.java
@@ -1,17 +1,9 @@
 package org.example.repository;
 
 import org.example.entity.CustomerEntity;
-import org.springframework.data.annotation.QueryAnnotation;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-
-import java.util.ArrayList;
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 //@EnableJpaRepositories
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
@@ -25,7 +17,12 @@ public interface CustomerRepository extends JpaRepository<CustomerEntity, Intege
     //@Query( value ="SELECT c FROM CustomerEntity c WHERE c.email=?1" )
     //above all 3 are ok. as default comes "nativeQuery=false"
     CustomerEntity findCustomerCodeByEmail(String email);
-
+    @Query(nativeQuery=false, value ="SELECT COUNT (c)>0 FROM CustomerEntity c WHERE c.email=?1")
     boolean existsByEmail (String mail);
+
+/*
+    @Query("SELECT COUNT(c) > 0 FROM CustomerEntity c WHERE c.email =:email2")
+    boolean existsByEmail(@Param("email2") String mail); //(@Param("email2")=Here defined the ":email2".
+*/
 
 }
